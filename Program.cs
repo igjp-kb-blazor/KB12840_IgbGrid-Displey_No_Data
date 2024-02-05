76f2920e18c98057c8efdedf05595b38a926b7b9
Tomoya Washio <TWashio@infragistics.com>
2024-02-05 15:29:04 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using KB12840_IgbGrid_Displey_No_Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor();
+
+await builder.Build().RunAsync();
